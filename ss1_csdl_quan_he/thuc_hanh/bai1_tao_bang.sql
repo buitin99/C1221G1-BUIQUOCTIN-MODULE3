49a61063636a657d286c97787d8758718f20c3c5
buitin99 <buiquoctin.job@gmail.com>
2022-03-28 10:09:11 +0700

    nop bai ss1

---
+++ ss1_csdl_quan_he/thuc_hanh/bai1_tao_bang.sql
@@ -0,0 +1,7 @@
+create database `student-management`;
+create table  `student-management`.`student`(
+`id` int not null,
+`name` varchar(45) null,
+`age` int null,
+`country` varchar(45) null,
+primary key (id));
