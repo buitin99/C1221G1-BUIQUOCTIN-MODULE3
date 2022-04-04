309ae7fda29e91ff1e332d66e1f4664d57ec40da
buitin99 <buiquoctin.job@gmail.com>
2022-04-04 21:11:37 +0700

    nop bai case study

--- case_study_data_base/casestudy.sql
+++ case_study_data_base/casestudy.sql
@@ -223,7 +223,7 @@ and (khach_hang.dia_chi like "%Đà Nẵng%" or khach_hang.dia_chi like "%Quản
 -- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
 -- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
 
-select khach_hang.ho_va_ten, count(hop_dong.ma_khach_hang) as so_lan_dat_phong from loai_khach right join khach_hang 
+select khach_hang.ho_va_ten, count(hop_dong.ma_khach_hang) as so_lan_dat_phong from loai_khach join khach_hang 
 on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang 
 where loai_khach.ten_loai_khach = 'Diamond' group by khach_hang.ho_va_ten having so_lan_dat_phong > 0 order by so_lan_dat_phong;
 
